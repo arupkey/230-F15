e5b3fe549f9b45a0f5205084aa048c45168b2f38
Andrew Rupkey <andrewrupkey@gmail.com>
2015-10-29 20:56:22 -0400

    no message

--- Stack/src/submission/Palindrome.java
+++ Stack/src/submission/Palindrome.java
@@ -1,8 +1,22 @@
 package submission;
 
-public class Palindrome {
-	public static boolean isPalindrome(String s) {
-		// TODO use a stack to determine if s is palindrome
+public class Palindrome<T> {
+	public char[] array;
+	public T[] reversearray;
+	
+	public boolean isPalindrome(String s) {
+		array=s.toCharArray();
+		for (int i=0; i<array.length; i++){
+			if (array[i]==array[array.length-1-i]){
+				return true;
+			}
+		}
 		return false;
 	}
+public static void main(String[] args){
+	Palindrome p =new Palindrome();
+	System.out.println(p.isPalindrome("amanaplanacanalpanama"));
+	System.out.println(p.isPalindrome("2002"));
+	System.out.println(p.isPalindrome("racecar"));
+}
 }
